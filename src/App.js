897e12a9d9bbe295cdd25692feed1015c506c9fe
felgamedev <felgamedev@gmail.com>
2018-08-21 21:10:45 -0300

    Initial implementation of Foursquare

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import escapeRegExp from 'escape-string-regexp'
 const fourSquareConfig = {
   'secrets': {
     'clientId': 'N3XZR40E5SSOTRPFD4RGBATEIBLPNHBUAUBN0LFXZCIQCW21',
-    'clientSecret' : 'T2PUDE4YKR03JDN5BD5AS4KW42IOHH2213W4WNKZ1MHG0GVT',
+    'clientSecret' : 'KKGGWYKP1OBVRMVLQYWQBU1XCYK1CKVJEBSWRYOTVQVZD4BZ',
     'redirectUrl': 'REDIRECT_URL'
   }
 }
@@ -20,39 +20,74 @@ const FarmMap = withScriptjs(withGoogleMap((props) =>
   </GoogleMap>
 ))
 
+const MapInfoWindow = (props) =>
+    <InfoWindow>
+      <div>
+        <div className="venue-data">
+
+        </div>
+        <div className="similar-venue-data">
+          {props.location && <h3>{props.location.title}</h3>}
+          {props.similarVenueData && <div>I have similarVenueData</div>}
+          {(props.similarVenueData && (props.similarVenueData.similarVenues.count > 0)) && <div>More than 0!</div>}
+        </div>
+
+      </div>
+    </InfoWindow>
+
 class App extends Component {
   state = {
     queryValue: "",
     allLocations: [
-      { title: "Battery Park Beer Bar", position: { lat: 44.6668493, lng: -63.5699491 }, venueId: '567579ee498e2f30a8cac694' },
-      { title: "Jost Vineyards", position: { lat: 45.796224, lng: -63.377246 }, venueId: '4be8181d947820a19271b4db' },
-      { title: "Blomidon Estate Winery", position: { lat: 45.1570488, lng: -64.3926467 }, venueId: '4b76ea9bf964a520466a2ee3' },
-      { title: "Avondale Sky Winery", position: { lat: 45.1124797, lng: -64.4008356 }, venueId: '503111bae4b0265f8c49d742' },
-      { title: "Gasperau Vineyards", position: { lat: 45.0688838,lng: -64.3592938 }, venueId: '4be6e0bb910020a1a9b5d414' },
-      { title: "Spindrift Brewing", position: { lat: 44.7141604, lng: -63.5852723}, venueId: '573c6c35498e9c48598a1ac7' },
-      { title: "Muwin Estates Wines", position: { lat: 44.8299832, lng: -64.5139131}, venueId: '4cf1588ae9425481f6bd69c5' },
+      { title: "Battery Park Beer Bar", position: { lat: 44.6668493, lng: -63.5699491 }, venueId: '567579ee498e2f30a8cac694', venueData: null },
+      { title: "Jost Vineyards", position: { lat: 45.796224, lng: -63.377246 }, venueId: '4be8181d947820a19271b4db', venueData: null },
+      { title: "Blomidon Estate Winery", position: { lat: 45.1570488, lng: -64.3926467 }, venueId: '4b76ea9bf964a520466a2ee3', venueData: null },
+      { title: "Gasperau Vineyards", position: { lat: 45.0688838,lng: -64.3592938 }, venueId: '4be6e0bb910020a1a9b5d414', venueData: null },
+      { title: "Avondale Sky Winery", position: { lat: 45.1124797, lng: -64.4008356 }, venueId: '503111bae4b0265f8c49d742', venueData: null },
+      { title: "Spindrift Brewing", position: { lat: 44.7141604, lng: -63.5852723}, venueId: '573c6c35498e9c48598a1ac7', venueData: null },
+      { title: "Muwin Estates Wines", position: { lat: 44.8299832, lng: -64.5139131}, venueId: '4cf1588ae9425481f6bd69c5', venueData: null },
     ],
     shownLocations: [],
     selectedLocation: null,
     mapCenterPosition: null,
     defaultCenter: {lat: 44.6463819, lng: -63.5912759 },
-    infoWindowOpen: false
+    infoWindowOpen: false,
+    venueFromFoursquare: null,
+    similarVenueData: null
+  }
+
+  // When a location is selected, load up similar locations to state for InfoWindow to reference
+  getFourSquareData = (location) => {
+    // Get general info about the venue
+    fetch(`https://api.foursquare.com/v2/venues/${location.venueId}?&client_id=${fourSquareConfig.secrets.clientId}&client_secret=${fourSquareConfig.secrets.clientSecret}&v=20180821`)
+    .then(res => res.json())
+    .then(data => this.setState({
+      venueFromFoursquare: data.response
+    }))
+
+    // Get data about similar locations
+    fetch(`https://api.foursquare.com/v2/venues/${location.venueId}/similar/?&client_id=${fourSquareConfig.secrets.clientId}&client_secret=${fourSquareConfig.secrets.clientSecret}&v=20180821`)
+    .then(res => res.json())
+    .then(data => this.setState({
+      similarVenueData: data.response
+    }))
   }
 
   componentWillMount(){
     // Copy the full list from the allLocations array
     let all = []
+
     for(let index in this.state.allLocations){
       all.push(this.state.allLocations[index])
     }
+
     // Copy this initial list to the shownLocations state too
     this.setState(state => ({
+      allLocations: all,
       shownLocations: all,
       mapCenterPosition: this.state.defaultCenter
     }))
   }
-
-
 
   selectLocation = (location) => {
     this.setState({
@@ -60,12 +95,17 @@ class App extends Component {
       mapCenterPosition: location.position,
       infoWindowOpen: true
     })
+
+    // Prevent calls during building of main app
+    this.getFourSquareData(location)
   }
 
   deselectLocation = () => {
     this.setState({
       selectedLocation: null,
-      infoWindowOpen: false
+      infoWindowOpen: false,
+      venueFromFoursquare: null,
+      similarVenueData: null
     })
   }
 
@@ -124,19 +164,17 @@ class App extends Component {
   render() {
     let {shownLocations, queryValue, mapCenterPosition, infoWindowOpen, selectedLocation} = this.state
     let markers = []
+
     for(let i = 0; i < shownLocations.length; i++){
       markers.push(<Marker key={shownLocations[i].title}
         title={shownLocations[i].title}
         position={shownLocations[i].position}
         onClick={() => this.onMarkerClicked(shownLocations[i])}>
         {(infoWindowOpen && selectedLocation === shownLocations[i]) &&
-            <InfoWindow onCloseClick={this.toggleInfoWindow}>
-              <div>
-                <h3>{shownLocations[i].title}</h3>
-              </div>
-            </InfoWindow>}
+            <MapInfoWindow onCloseClick={this.toggleInfoWindow} location={shownLocations[i]} similarVenueData={this.state.similarVenueData} />}
       </ Marker>)
     }
+
     return (
       <div className="app-container">
         <div className="side-bar">
